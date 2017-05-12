24cde631dd612c050305bad3de042882fabc5e63
ashwinarasu <arasuashwin@gmail.com>
2017-05-12 10:40:52 -0700

    Create and add name field to SQLite database and create text field and button to add contact to database

--- MyContactApp2/app/src/main/java/com/example/arasua6707/mycontactapp/DatabaseHelper.java
+++ MyContactApp2/app/src/main/java/com/example/arasua6707/mycontactapp/DatabaseHelper.java
@@ -4,6 +4,7 @@ package com.example.arasua6707.mycontactapp;
  * Created by arasua6707 on 5/11/2017.
  */
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -14,17 +15,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COL_1 = "ID";
     public static final String COL_2 = "NAME";
 
-    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, DATABASE_NAME, null, version);
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID_INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
     }
 }
