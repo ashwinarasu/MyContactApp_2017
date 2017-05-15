147bc9dfaf24f45396d735202687309abcb147ac
ashwinarasu <arasuashwin@gmail.com>
2017-05-15 11:10:02 -0700

    made some toast and created addData and insertData methods

--- MyContactApp2/app/src/main/java/com/example/arasua6707/mycontactapp/DatabaseHelper.java
+++ MyContactApp2/app/src/main/java/com/example/arasua6707/mycontactapp/DatabaseHelper.java
@@ -3,10 +3,12 @@ package com.example.arasua6707.mycontactapp;
 /**
  * Created by arasua6707 on 5/11/2017.
  */
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
@@ -21,12 +23,25 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID_INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT)");
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+    public boolean insertData(String name){
+        Log.d("MyContact", "got to insert data " + name);
+        SQLiteDatabase db = this.getWritableDatabase();
+        Log.d("MyContact", "after line");
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+
+        Log.d("MyContact", "setup content value");
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result == -1) return false;
+        else return true;
     }
 }
--- MyContactApp2/app/src/main/java/com/example/arasua6707/mycontactapp/MainActivity.java
+++ MyContactApp2/app/src/main/java/com/example/arasua6707/mycontactapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.arasua6707.mycontactapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +22,34 @@ public class MainActivity extends AppCompatActivity {
 
         myDb = new DatabaseHelper(this);
 
+        //Add the layout vars
+        editName = (EditText) findViewById(R.id.editText_name);
+    }
+
+    public void addData(View v){
+        Log.d("MyContact", "Successful data insertion");
+        boolean isInserted = myDb.insertData(editName.getText().toString());
+
+
+
+        if (isInserted == true){
+            Log.d("MyContact", "Successful data insertion");
+            Context context = MainActivity.this;
+            CharSequence text = "THE DATA IS IN! NOW BUTTER THAT TOAST!";
+           int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+        else{
+            Log.d("MyContact", "Data insertion failure");
+            Context context = getApplicationContext();
+            CharSequence text = "YOUR TOAST WAS BURNT :(";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
+        }
     }
 }
